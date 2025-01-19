831be4bd3f448138e6d673cef1d25e3c21731292
luis antonio <tyluis20@gmail.com>
2025-01-18 23:21:43 -0300

    10# implementar infra kubernetes

---
+++ deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Nome do cluster e arquivo de configuração do Kind
+CLUSTER_NAME="tech-challenge"
+KIND_CONFIG="kind-config.yaml"
+
+# Diretório contendo os arquivos YAML
+YAML_DIR="infra/app"
+ls -l $YAML_DIR
+# Função para verificar o status do último comando e exibir mensagens apropriadas
+check_status() {
+  if [ $? -eq 0 ]; then
+    echo "Sucesso: $1"
+  else
+    echo "Erro: $1"
+    exit 1
+  fi
+}
+
+# Passo 1: Criar o cluster com o Kind
+echo "Criando o cluster '$CLUSTER_NAME'..."
+kind create cluster --name "$CLUSTER_NAME" --config "infra/kind/$KIND_CONFIG"
+check_status "Cluster '$CLUSTER_NAME' criado com sucesso."
+
+# Passo 2: Verificar se o diretório de arquivos YAML existe
+if [ ! -d "$YAML_DIR" ]; then
+  echo "Erro: Diretório $YAML_DIR não encontrado."
+    mkdir -p "$YAML_DIR"
+  exit 1
+fi
+
+# Passo 3: Aplicar cada arquivo YAML individualmente e registrar o status
+for yaml_file in "$YAML_DIR"/*.yaml; do
+  if [ -f "$yaml_file" ]; then
+    echo "Aplicando $yaml_file..."
+    kubectl apply -f "$yaml_file"
+    check_status "Aplicação de $yaml_file concluída."
+  else
+    echo "Aviso: Nenhum arquivo YAML encontrado em $YAML_DIR."
+  fi
+done
+
+echo "Processo de implantação concluído com sucesso."
+echo "ficara ativa em alguns segundos"
+
+kubectl get pods -w 
+
+#//fica roando o ccurl para verificar se o serviço esta ativo
+while [ $(kubectl get pods -o jsonpath='{.items[*].status.containerStatuses[*].ready}' | grep false | wc -l) -gt 0 ]; do
+  echo "Aguardando a inicialização dos pods..."
+  sleep 5
+done
+curl  http://localhost:30000/
